8f8c4373b869e12862fa71db22e3d3d4ce618fd3
Adnan Ganie <adnan.ayoub@lelafe.com>
2023-12-27 23:04:46 +0530

    banners added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core'
+import { Swiper } from 'swiper'
+import { register } from 'swiper/element/bundle'
 
+register()
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined
+  swiper?: Swiper
   constructor() {}
 
+  onSwiperReady() {
+    this.swiper = this.swiperRef?.nativeElement.swiper
+  }
+
+  onSlideChange($event: any) {
+    console.log($event)
+  }
 }
